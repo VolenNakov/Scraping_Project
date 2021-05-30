bb80de8839426d6055fc086b917302218fcf0a77
Volen Nakov <volen.nakov@gmail.com>
2021-05-31 00:41:28 +0300

    Implemented a way to scrap some of the btv news, lots of TODOS left

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
 #Initial coomit
+import requests
+from bs4 import BeautifulSoup as bs
+
+URL = 'https://btvnovinite.bg/'
+page = requests.get(URL)
+
+soup = bs(page.content, 'html.parser')
+results = soup.find(id='content_router')
+news_elems = results.find_all('li', class_='item')
+for news_elem in news_elems:
+    title_elem = news_elem.find('div', class_='title')
+    link_elem = news_elem.find('a', class_='link')
+    img_elem = news_elem.find('img', src_='src')
+    text_elem = news_elem.find('div', class_='text')
+    print(title_elem)
+    print(link_elem)
+    print(img_elem)
+    print(text_elem)
+    print()
