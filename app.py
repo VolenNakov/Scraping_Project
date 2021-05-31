647ee7af52a5bbaae43e385bbf1a08939ce0c542
Volen Nakov <volen.nakov@gmail.com>
2021-06-01 00:32:01 +0300

    Added class to the program. Now everything is stored in the news list.

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
-#Initial coomit
 import requests
 from bs4 import BeautifulSoup as bs
+
+class News_BTV:
+    def __init__(self,title,link,img):
+        self.title = title
+        self.link = link
+        self.img = img
 
 URL = 'https://btvnovinite.bg/'
 page = requests.get(URL)
 
 soup = bs(page.content, 'html.parser')
 results = soup.find(id='content_router')
+
 news_elems = results.find_all('li', class_='item')
+news_list = []
+
 for news_elem in news_elems:
     title_elem = news_elem.find('div', class_='title')
     link_elem = news_elem.find('a', class_='link')
-    img_elem = news_elem.find('img', src_='src')
-    text_elem = news_elem.find('div', class_='text')
-    print(title_elem)
-    print(link_elem)
-    print(img_elem)
-    print(text_elem)
-    print()
+    img_elem = news_elem.find('img')
+    news_list.append(News_BTV(title_elem,link_elem,img_elem))
