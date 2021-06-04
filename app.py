47d494e2a87ec097eb6d02e6c9e13af92ccf4354
Volen Nakov <volen.nakov@gmail.com>
2021-06-05 00:42:37 +0300

    Now the app can scrape all of the news on the main page. Next thing to implement is to save the text in the news.

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 import requests
+import sqlite3
+import uuid
 from bs4 import BeautifulSoup as bs
+
+con = sqlite3.connect('news.db')
+cur = con.cursor()
+URL = 'https://btvnovinite.bg/'
 
 class News_BTV:
     def __init__(self,title,link,img):
@@ -7,17 +13,33 @@ class News_BTV:
         self.link = link
         self.img = img
 
-URL = 'https://btvnovinite.bg/'
-page = requests.get(URL)
+def scrapMainPage():
+    page = requests.get(URL)
+    
+    soup = bs(page.content, 'html.parser')
+    results = soup.find(id='content_router')
 
-soup = bs(page.content, 'html.parser')
-results = soup.find(id='content_router')
+    news_elems = results.find_all('li', class_='item')
+    for news_elem in news_elems:
+        title_elem = news_elem.find('div', class_='title')
+        link_elem = news_elem.find('a', class_='link')
+        img_elem = news_elem.find('img')
+        if title_elem is not None and link_elem is not None and img_elem is not None:
+            Object = News_BTV(title_elem.text,link_elem['href'],img_elem['src'])
+            addToDb(Object)
 
-news_elems = results.find_all('li', class_='item')
-news_list = []
+def addToDb(element):
+    id = uuid.uuid3(uuid.NAMESPACE_URL, "https://btvnovinite.bg/"+element.link)
+    if cur.execute("SELECT EXISTS(SELECT 1 from news WHERE id='"+str(id)+"')").fetchone():
+        print("This Id exsists: "+str(id))
+    else:
+        for data in [(str(id),str(element.title),str(element.link),str(element.img))]:
+            print(id)
+            cur.execute('INSERT INTO news VALUES (?,?,?,?)',data)
+            con.commit()
 
-for news_elem in news_elems:
-    title_elem = news_elem.find('div', class_='title')
-    link_elem = news_elem.find('a', class_='link')
-    img_elem = news_elem.find('img')
-    news_list.append(News_BTV(title_elem,link_elem,img_elem))
+def main():
+    scrapMainPage()
+
+if __name__ == "__main__":
+    main()
