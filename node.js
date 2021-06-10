74af2863d509c64232107227be07d90b3bb111bc
Volen Nakov <volen.nakov@gmail.com>
2021-06-11 00:32:15 +0300

    Pushing stjo's changes

--- node.js
+++ node.js
@@ -17,31 +17,17 @@ class Article {
   }
 }
 
-
-function createList(array) {
-  db.serialize(() => {
-    db.each('SELECT href as href, title as title FROM news', (err, row) => {
-      if (err) {
-        console.error(err);
-      }
-      else {
-        array.push(new Article(row.href, row.title))
-      }
-    });
-  })
-};
 app.get('/', (req, res) => {
   let html = "";
-  let articles = []
-  createList(articles)
-  console.log(articles)
-  for (let i = 0; i < articles.length; i++) {
-    html += '<p><a href="' + articles[i].href + '">' + articles[i].title + '</a><br>'
-  };
-  res.send(html)
+	db.all('SELECT href, title FROM news', (err, articles) => {
+		// тук вече имаме данните
+		for (let i = 0; i < articles.length; i++) {
+			html += '<p><a href="' + articles[i].href + '">' + articles[i].title + '</a><br>'
+		};
+		res.send(html)
+	});
 });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 });
-
