e0c56644aa6e28d383fb9eab05c385dfb34e3888
Volen Nakov <volen.nakov@gmail.com>
2021-06-10 09:10:13 +0300

    Idfk what's in this

--- node.js
+++ node.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 const sqlite3 = require('sqlite3').verbose();
-let html = ""
+
 let db = new sqlite3.Database('news.db', sqlite3.OPEN_READONLY, (err) => {
   if (err) {
     return console.error(err.message);
@@ -10,17 +10,34 @@ let db = new sqlite3.Database('news.db', sqlite3.OPEN_READONLY, (err) => {
   console.log('Connected databse')
 });
 
+class Article {
+  constructor(href, title) {
+    this.href = href;
+    this.title = title;
+  }
+}
 
-app.get('/', (req, res) => {
+
+function createList(array) {
   db.serialize(() => {
-    db.each('SELECT href as href,title as title FROM news', (err, row) => {
+    db.each('SELECT href as href, title as title FROM news', (err, row) => {
       if (err) {
-        console.error(err.message);
+        console.error(err);
       }
-      html+='<p><a href="'+row.href+'">'+row.title+'</a><br>'
+      else {
+        array.push(new Article(row.href, row.title))
+      }
     });
-  });
-  console.log(html)
+  })
+};
+app.get('/', (req, res) => {
+  let html = "";
+  let articles = []
+  createList(articles)
+  console.log(articles)
+  for (let i = 0; i < articles.length; i++) {
+    html += '<p><a href="' + articles[i].href + '">' + articles[i].title + '</a><br>'
+  };
   res.send(html)
 });
 
