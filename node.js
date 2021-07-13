2df756746282af52ea3b38ccbf86676f78412f24
Volen Nakov <volen.nakov@gmail.com>
2021-07-14 00:32:06 +0300

    Lost track of what I changed in the last 3 days.... Still lot work to do ;)

--- node.js
+++ node.js
@@ -1,39 +1,52 @@
-const express = require('express')
-const app = express()
-const port = 3000
+'use strict';
+const express = require('express');
+const app = express();
+const port = 3000;
 const sqlite3 = require('sqlite3').verbose();
 
 let db = new sqlite3.Database('news.db', sqlite3.OPEN_READONLY, (err) => {
   if (err) {
     return console.error(err.message);
-  }
-  console.log('Connected to database')
+  };
+  console.log('Connected to database');
 });
-
-class Article {
-  constructor(href, title) {
-    this.href = href;
-    this.title = title;
-  }
-}
 
 app.get('/', (req, res) => {
-  let html = "";
-	db.all('SELECT id, img_src, title FROM news', (err, articles) => {
-		// тук вече имаме данните
-		for (let i = 0; i < articles.length; i++) {
-			html += '<p><a href="' + articles[i].id + '">' + articles[i].title + '</a><img src='+articles[i].img_src+' width="200" height="100"><br>'
-            
-		};
-		res.send(html)
-	});
+  let html = "<div class='text'> ";
+  db.all('SELECT news.id, news.img_src, news.href, news.title FROM news INNER JOIN articles ON news.id = articles.id WHERE articles.text != "null"', (err, articles) => {
+    // тук вече имаме данните
+    for (let i = 0; i < articles.length; i++) {
+      html += '<p><a href="' + articles[i].id + '">' + articles[i].title + '</a><img src=' + articles[i].img_src + ' width="200" height="100"><br>';
+    };
+    html += "</div>"
+  });
+  db.all('SELECT news.id, news.img_src, news.href, news.title FROM news INNER JOIN articles ON news.id = articles.id WHERE articles.text == "null"',(err,articles)=>{
+    html += "<div class='textless'> "
+    for (let i = 0; i < articles.length; i++) {
+      html += '<p><a href="' + articles[i].id + '">' + articles[i].title + '</a><img src=' + articles[i].img_src + ' width="200" height="100"><br>';
+    };
+    html += "</div>"
+    res.send(html);
+  });
 });
 app.get('/:articleid', (req, res) => {
-    let html = '';
-    console.log(req.params['articleid']);
-    res.send(html) 
+  let html = '<span style="white-space: pre-line">';
+  db.all('SELECT text FROM articles WHERE id = ?', req.params['articleid'], (err, text) => {
+    try {
+      if (text[0].text) {
+        html += text[0].text;
+      };
+    }
+    catch (e) {
+      html += "There's nothing here";
+    };
+    text += '</span>';
+    res.send(html);
+  });
+  console.log(req.params['articleid']);
 });
+
 app.get('/favicon.ico', (req, res) => res.status(204));
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`);
 });
--- remake.py
+++ remake.py
@@ -1,9 +1,10 @@
 import requests
 import sqlite3
 import uuid
-import datetime
+from datetime import datetime
 from bs4 import BeautifulSoup as bs
 
+debug = False
 con = sqlite3.connect("news.db")
 cur = con.cursor()
 URL = "https://btvnovinite.bg"
@@ -17,11 +18,7 @@ for row in records:
 
 def convertTime(time):
     time = time.replace("ч.", "").replace("г.", "")
-    year = time[13:17]
-    month = time[10:12]
-    day = time[7:9]
-    hour = time[0:2]
-    minute = time[3:5]
+    year, month, day, hour, minute = time[13:17], time[10:12], time[7:9], time[0:2], time[3:5]
     return year + "-" + month + "-" + day + " " + hour + ":" + minute + ":00"
 
 
@@ -31,42 +28,62 @@ def getArticles():
     results = soup.find(class_="list-wrapper")
 
     articles = results.find_all("li", class_="item")
+    
     for article in articles:
-        title = article.find("div", class_="title").text
-        link = article.find("a", class_="link")["href"]
-        img = article.find("img")["src"]
+        title, link, img= article.find("div", class_="title").text, article.find("a", class_="link")["href"], article.find("img")["src"]
         id_ = uuid.uuid3(uuid.NAMESPACE_URL, URL + link)
         if str(id_) not in ids:
             for data in [(str(id_), str(title), str(link), str(img))]:
                 cur.execute("INSERT INTO news VALUES (?,?,?,?)", data)
                 con.commit()
+            cur.execute("INSERT INTO articles VALUES (?,?,?,?)",
+                        (str(id_), "NULL", "NULL","NULL"))
+            con.commit()
         else:
-            print(
-                "This article already exists:"
-                + "\n    - "
-                + str(id_)
-                + "\n    - "
-                + str(title)
-            )
+            if debug:
+                print(
+                    "This article already exists:"
+                    + "\n    - "
+                    + str(id_)
+                    + "\n    - "
+                    + str(title)
+                )
 
 
 def scrapeArticle(link):
     page = requests.get(URL + link)
-    soup = bs(page.text, "html.parser")
-    body = soup.find(class_="article-body")
-    text = ""
-    for tag in body:
-        if tag.name == "p":
-            text += tag.text + "\n"
-    text = text.replace(u"\xa0", u" ")
-    data = (str(text),str(uuid.uuid3(uuid.NAMESPACE_URL, URL + link)))
-    cur.execute("UPDATE news SET text = ? WHERE id= ?",data)
-    con.commit()
+    if page.url == URL:
+        if debug:
+            print("This article is deleted - "+link)
+    else:
+        soup = bs(page.text, "html.parser")
+        try:
+            modified, tags = soup.find("span", class_="modified"), [tag.text for tag in soup.find(class_='keywords-wrapper').findAll('a')]
+        except AttributeError:
+            tags = "None"
+        id_ = str(uuid.uuid3(uuid.NAMESPACE_URL, URL + link))
+        check = cur.execute(
+            '''SELECT modified FROM articles WHERE id = ?''', (id_,)).fetchall()
+        if check[0][0] == 'NULL':
+            body = soup.find(class_="article-body")
+            text = ' '.join(map(str,[tag.text + '\n' for tag in body if tag.name == 'p']))
+            cur.execute('''UPDATE articles SET modified = ?, text = ? WHERE id =?''',
+                        (convertTime(modified.text), text, id_))
+            con.commit()
+        elif datetime.strptime(check[0][0], "%Y-%m-%d %H:%M:%S") < datetime.strptime(convertTime(modified.text), "%Y-%m-%d %H:%M:%S"):
+            body = soup.find(class_="article-body")
+            print("This is changed -"+link+id_)
+            print(' '.join(map(str,[tag.text + '\n' for tag in body if tag.name == 'p'])))
+            #cur.execute('''UPDATE articles SET modified = ? WHER id = ?''',(convertTime(modified.text),id_))
+            # con.commit(0)
+
 
 def main():
     getArticles()
-    link = "/bulgaria/novozarazenite-s-indijskija-variant-na-covid-19-rabotjat-v-stolichnija-gradski-transport.html"
-    scrapeArticle(link)
+    cur.execute('SELECT href FROM news')
+    links = cur.fetchall()
+    for link in links:
+        scrapeArticle(link[0])
 
 
 if __name__ == "__main__":
